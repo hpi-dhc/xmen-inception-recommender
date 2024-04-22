065c8c130f15efc746262664d2880c78e7dc455c
Florian Borchert <Florian.Borchert@hpi.de>
2024-04-22 16:08:16 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# xmen-inception-recommender
+# xMEN Based Entity Linking Recommender for INCEpTION
+
+## German SNOMED Linking
+
+- Prepare xMEN KB and index as described here: https://github.com/hpi-dhc/xmen/blob/main/examples/03_SNOMED_Linking_German.ipynb
+- run `python run_snomed_german_recommender.py <path to xmen index folder>`
---
+++ run_snomed_german_recommender.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from cassis import Cas
+import argparse
+
+from ariadne.server import Server
+from ariadne.classifier import Classifier
+from ariadne.contrib.inception_util import create_span_prediction
+
+from xmen.linkers import default_ensemble
+from xmen.linkers import EntityLinker
+
+class xMENSNOMEDLinker(Classifier):
+    def __init__(self, linker: EntityLinker, top_k = 10):
+        self.last_cas = None
+        self.linker = linker
+        self.top_k = top_k        
+        super().__init__()
+        
+    def predict(self, cas: Cas, layer: str, feature: str, project_id: str, document_id: str, user_id: str):
+        self.last_cas = cas
+        # For every annotated SNOMED span, predict the SNOMED code
+        for s in cas.select('webanno.custom.SNOMED'):
+            text = s.get_covered_text()
+            pred = self.linker.predict_no_context(text)
+            for concept in pred['normalized'][0:self.top_k]:
+                sctid = concept['db_id']
+                prediction = create_span_prediction(cas, layer, feature, s.begin, s.end, f"http://snomed.info/id/{sctid}", score=concept['score'])
+                cas.add(prediction)     
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('index_base_path', type=Path)
+    args = parser.parse_args()
+    index_base_path = args.index_base_path
+    linker = default_ensemble(index_base_path, cuda=False)
+  
+    recommender = xMENSNOMEDLinker(linker)
+
+    server = Server()
+    server.add_classifier("xmen_snomed", recommender)
+
+    server.start()
+
+if __name__ == '__main__':
+    run()
